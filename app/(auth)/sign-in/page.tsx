98d68f9aba9959c0d6d3a661373f2ccf1d5a9e23
Soma Shekar <somashekar2025@gmail.com>
2025-10-30 23:23:12 +0530

    implement auth logic

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,9 +3,13 @@
 import FooterLink from "@/components/forms/FooterLink";
 import InputField from "@/components/forms/InputField";
 import { Button } from "@/components/ui/button";
+import { signInWithEmail } from "@/lib/actions/auth.actions";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 const SignIn = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -19,13 +23,17 @@ const SignIn = () => {
     mode: "onBlur",
   });
 
-  const onSubmit = async (data: SignInFormData) => {
-    try {
-      console.log(data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  const onSubmit = async (data: SignInFormData) =>  {
+      try {
+        const result = await signInWithEmail(data);
+        if(result.success) router.push('/');
+      } catch (e) {
+        console.error(e);
+        toast.error('Sign in failed', {
+          description: e instanceof Error ? e.message : 'Failed to sign in.'
+        })
+      }
+    };
 
   return (
     <>
