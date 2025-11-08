d9a0a2f5b06c412028f6a4511a3d1ec0f9a2092f
Soma Shekar <somashekar2025@gmail.com>
2025-11-08 16:24:20 +0530

    "update dashboard-preview image"

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -51,7 +51,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
         </div>
         
         <div className="flex-1 relative">
-          <Image src="/assets/images/dashboard.png" alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute top-0" />
+          <Image src="/assets/images/dashboard-preview1.png" alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute top-0" />
         </div>
       </section>
     </main>
